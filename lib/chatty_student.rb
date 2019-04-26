bcfc4ba37e8fdeeec7ac43ebf7e9b4beb66967c9
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-26 00:28:45 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,7 +2,10 @@ class ChattyStudent < Student
 
   def hello
     super
-    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n" to stdout, but output "Hey there! I'm so excited to learn stuff.\nHey there! I'm so excited to learn stuff.\nHow are you do... was so crazy! What, you don't want any spoilers? Okay well\n    let me just tell you who died...\n"
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it w
+as so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n" to stdout, but output "Hey there! I'm so excited
+to learn stuff.\nHey there! I'm so excited to learn stuff.\nHow are you do... was so crazy! What, you don't want any spoilers? Okay well\n    l
+et me just tell you who died...\n"
   end
 
   def raise_hand
